ea67179e9ba6e67ed351285ce6aa7e0905dc9aa3
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-19 21:15:48 +0200

    Created a side bar component, which will be used to go home and favourites.

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdHomeFilled } from "react-icons/md";
+import { FaHeart } from "react-icons/fa";
+import logo from "../assets/images/podcast.png"
+
+
+
+function SideBar() {
+  return (
+    <div className="side-bar">
+      <img src={logo} alt="Logo" className='logo'/>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">
+              <MdHomeFilled/>
+              <span>  </span>
+              <p>Podcasts</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/favorites">
+              <FaHeart/>  Favorites
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default SideBar;
