02eaa528c904357937c55fbbbc555e7d507a17aa
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-19 22:14:04 +0200

    Added my Router and also added my sideBar and header which will be seperated using the parent div, I also imported BrowserRouter, Header and SideBar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import SideBar from './components/SideBar'
 import './App.css'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <div>
+        <SideBar />
+        <div>
+          <Header />
+        </div>
+      </div>
+    </Router>
   )
 }
 
