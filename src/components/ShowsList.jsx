22b202721fb85528bc2fac19adf6d127c9485a55
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-20 22:12:52 +0200

    Imported my sortbuttons component which will be used to handle sorting and re-rendering my shows.

--- src/components/ShowsList.jsx
+++ src/components/ShowsList.jsx
@@ -1,32 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import SortButtons from './SortButtons';
 import genreMappings from '../genreMapping';
 import "../Styles/showList.css"
 
 function ShowList() {
     const [shows, setShows] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [sortedShows, setSortedShows] = useState([]);
+    const [selectedSort, setSelectedSort] = useState('AZ');
 
     useEffect(() => {
         fetch('https://podcast-api.netlify.app')
           .then(response => response.json())
           .then(data => {
-            setShows(data);
+            const sortedData = data.sort((a, b) => a.title.localeCompare(b.title));
+            setShows(sortedData);
+            setSortedShows(sortedData);
             setLoading(false);
           })
           .catch(error => console.error('Error fetching shows:', error));
     }, []);
 
+    const handleSort = (sortedShows) => {
+        setSortedShows(sortedShows);
+    }
+
+    const handleSelectSort = (sortType) => {
+        setSelectedSort(sortType);
+    };
+
     if (loading) {
         return <div>Loading...</div>;
     }
-    
 
     return (
         <div className='show-list'>
+            <div className='controls'>
+                <SortButtons shows={shows} onSort={handleSort} selectedSort={selectedSort} onSelectSort={handleSelectSort} />
+            </div>
             <h1>All Shows</h1>
             <div className='category'>
-                {shows.map(show => (
+                {sortedShows.map(show => (
                 <div className='category-info' key={show.id}>
                     <Link to={`/show/${show.id}`}>
                     <div className='img-div'>
